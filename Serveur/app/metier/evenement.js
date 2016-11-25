9e81a76b792f2c9cb4686762452b0565eca9b733
Aurore <aurore.queille@free.fr>
2016-11-25 17:13:08 +0100

    Créer evenement + créer créneau

---
+++ Serveur/app/metier/evenement.js
@@ -0,0 +1,13 @@
+// Liste des evenements
+var listeEvenements = {};
+
+// Constructeur pour les Evenements
+function Evenement(id, nom, description) {
+    // l'id de levenement
+    this.id = id;
+    // nom de l'evenement
+    this.nom = nom;
+    // la description de l'evenement
+    this.description = description;
+}
+
