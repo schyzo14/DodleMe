95e10974dcdf00e8f78778444801aee8eba7c9a5
Manon <manon.fabarez@gmail.com>
2016-11-25 17:15:33 +0100

    Evenement : affichage et classe métier

---
+++ Serveur/app/metier/evenement.js
@@ -0,0 +1,21 @@
+var listeEvenement = {};
+
+function Evenement(id, nom, descriptif){
+    this.id = id;
+    this.nom = nom;
+    this.descriptif = nom;
+}
+
+// créer un nouveau événement
+var creerEveneement = function(id, nom, descriptif) {
+	// s'il n'existe pas
+	if (typeof listeEvenement[id] === 'undefined') {
+		// ajout de l'evenement
+		listeEvenement[id] = new Evenement(id, nom, descriptif);
+		return 1;
+    }
+    return 0;
+}
+
+exports.creerEvenement = creerEvenement;
+exports.getListe = getListe;
