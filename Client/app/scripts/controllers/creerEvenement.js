009c406404692a00602fd4a53040b6563401cc6a
Aurore <aurore.queille@free.fr>
2016-11-29 11:40:57 +0100

    creerEvenement

--- Client/app/scripts/controllers/creerEvenement.js
+++ Client/app/scripts/controllers/creerEvenement.js
@@ -11,32 +11,13 @@ angular.module('clientApp')
   .controller('CreerEvenementCtrl', ['$scope', 'eventFactory', '$location', function ($scope, eventFactory) {
     //eventFactory.post();
       
+    $scope.data = {};
+      
     $scope.creerEvenementF = function (form) {
-        alert("form : "+form);
         
-        if (form.$valid) {
-            // Copier l'instance du flux 
-            var form = angular.copy(form);
-            
-            
-        }
+        var formV = angular.copy(form);
+        alert($scope.data.nom +" - " + $scope.data.descriptif + " - " + $scope.data.date);
+        
     };
-    
- $scope.dateController = function () {
-     alert("bla !")
-    $scope.myDate = new Date();
-    $scope.minDate = new Date(
-       $scope.myDate.getFullYear(),
-       $scope.myDate.getMonth() - 2,
-       $scope.myDate.getDate());
-    $scope.maxDate = new Date(
-       $scope.myDate.getFullYear(),
-       $scope.myDate.getMonth() + 2,
-       $scope.myDate.getDate());
-    $scope.onlyWeekendsPredicate = function(date) {
-       var day = date.getDay();
-       return day === 0 || day === 6;
-    }
- }; 
       
 }]);
