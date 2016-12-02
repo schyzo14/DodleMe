7f8d19dfa923d64ebbb8dbc875ab82c599a9782c
Manon <manon.fabarez@gmail.com>
2016-12-02 17:21:39 +0100

    V2 : Profil

--- Serveur/app/ressources/profil.js
+++ Serveur/app/ressources/profil.js
@@ -1,9 +1,12 @@
-var evenement = require('../metier/utilisateur.js');
+var evenement = require('../metier/profil.js');
 var express = require('express');
 var router = express.Router();
 
-
-
-
+//POST
+router.post(
+    '/', function(req,res){
+        req.json(profil.creerProfil(req.body.nom, req.body.prenom, req.body.nomUtil));
+    }
+);
 
 module.exports = router;
