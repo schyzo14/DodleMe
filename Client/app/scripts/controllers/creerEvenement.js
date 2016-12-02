88700a334b2076436a34e16bd69a273f213eb9bf
Aurore <aurore.queille@free.fr>
2016-12-02 22:59:59 +0100

    Créer Evenement --> OK

--- Client/app/scripts/controllers/creerEvenement.js
+++ Client/app/scripts/controllers/creerEvenement.js
@@ -8,22 +8,23 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('CreerEvenementCtrl', ['$location', '$scope', 'eventFactory', '$http', function ($location, $scope, eventFactory, $http) {
+  .controller('CreerEvenementCtrl', ['$location', '$scope', 'eventFactory', 'creneauFactory', function ($location, $scope, eventFactory, creneauFactory) {
     
-    /** Calendrier **/
+    /** Calendrier : Date du jour **/
     $scope.today = function() {
         $scope.dt = new Date();
     };
     $scope.today();
     
-    /** Mettre la date dans le tableau **/
+      
+    /** Mettre la date sur laquelle on a cliquée dans le tableau **/
     
     $scope.listDates = [];
     var cpt=0;
       
     $scope.$watch('dt',function()
     {     
-        /** Pas le jour présélectionné **/
+        /** Pas le jour présélectionné (aujourd'hui) **/
         if (cpt != 0) {
             /** Pas de duplication des dates **/
             var sorted_listDates = $scope.listDates.slice().sort();
@@ -40,17 +41,7 @@ angular.module('clientApp')
         }
         cpt = cpt+1;
     });
-    
-      
-    /** Bouton supprimer date **/
-    $scope.supprimer = function (dateP) {
-        for (var i = 0; i < $scope.listDates.length; i++) {
-            if ($scope.listDates[i].toString().trim() === dateP.toString().trim()) {
-                delete $scope.listDates[i];
-            }
-        }
-    }
-    
+        
     
     /** Bouton créer l'évènement **/
       
@@ -84,95 +75,82 @@ angular.module('clientApp')
                 if (taille === 0) {
                     alert ("Merci de mettre au moins un créneau !");
                 } else {
-                    var evCree = function(data){
-                        alert("L'évènement a été créé !");
-
-                            var idE = data;
-
-                            var arrayLignes = document.getElementById("tableDate").rows;
-                            var longueur = arrayLignes.length;
-                            /* Parcours des lignes du tableau */
-                            for(var i=0; i<longueur; i++) {
-                                var arrayColonnes = arrayLignes[i].cells;
-                                var date = arrayColonnes[0].innerHTML;
-                                var creneau1 = arrayColonnes[1].children[0].options[arrayColonnes[1].children[0].selectedIndex].value;
-                                var creneau2 = arrayColonnes[2].children[0].options[arrayColonnes[2].children[0].selectedIndex].value;
-                                var creneau3 = arrayColonnes[3].children[0].options[arrayColonnes[3].children[0].selectedIndex].value;
-                                if (creneau1 !== "Pas de créneau choisi") {
-                                    /** ajouter creneau 1 **/
-                                    $http({
-                                         method: 'POST',
-                                         url: 'http://localhost:8080/creneau/'+idE,
-                                         headers: {
-                                           'Content-Type': 'application/json'
-                                         },
-                                         data: { 
-                                               heure: creneau1,
-                                               date: date
-                                        }}).
-                                        success(function(data, status){
-                                            alert("Le créneau " + date + " - " + creneau1 + " créé !" );
-                                        }).
-                                        error(function(data, status){
-                                            alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau1);
-                                        });
-
-                                }
-                                if (creneau2 !== "Pas de créneau choisi") {
-                                    alert(date + " - "+ creneau2);
-                                    /** ajouter creneau 2 **/
-                                    $http({
-                                         method: 'POST',
-                                         url: 'http://localhost:8080/creneau/'+idE,
-                                         headers: {
-                                           'Content-Type': 'application/json'
-                                         },
-                                         data: { 
-                                               heure: creneau2,
-                                               date: date
-                                        }}).
-                                        success(function(data, status){
-                                            alert("Le créneau " + date + " - " + creneau2 + " créé !" );
-                                        }).
-                                        error(function(data, status){
-                                            alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau2);
-                                        });
-                                }
-                                if (creneau3 !== "Pas de créneau choisi") {
-                                    alert(date + " - "+ creneau3);
-                                    /** ajouter creneau 3 **/
-                                    $http({
-                                         method: 'POST',
-                                         url: 'http://localhost:8080/creneau/'+idE,
-                                         headers: {
-                                           'Content-Type': 'application/json'
-                                         },
-                                         data: { 
-                                               heure: creneau3,
-                                               date: date
-                                        }}).
-                                        success(function(data, status){
-                                            alert("Le créneau " + date + " - " + creneau3 + " créé !" );
-                                        }).
-                                        error(function(data, status){
-                                            alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau3);
-                                        });
-                                }
-                            }
-                            /** redirection page de détail **/
-                            $location.path('/detailEvenements/'+idE);
-                    }
-                
+                    
+                    /** on fait une factory avec les paramètres **/
                     var event = new eventFactory({
                         nom : $scope.data.nom,
                         descriptif: $scope.data.descriptif
                     });
+                    /** on fait un post pour créer l'évènement **/
                     event.$save(function success(data){
                         evCree(data.id);
                     }, function error(){
                         alert("Echec lors de la création de l'évènement !");
-                    })
+                    });
                     
+                    /** L'évènement a été créé donc on ajoute les créneaux **/
+                    var evCree = function(data){
+                        alert("L'évènement a été créé !");
+
+                        var idE = data;
+
+                        var arrayLignes = document.getElementById("tableDate").rows;
+                        var longueur = arrayLignes.length;
+                        /* Parcours des lignes du tableau */
+                        for(var i=0; i<longueur; i++) {
+                            var arrayColonnes = arrayLignes[i].cells;
+                            var date = arrayColonnes[0].innerHTML;
+                            var creneau1 = arrayColonnes[1].children[0].options[arrayColonnes[1].children[0].selectedIndex].value;
+                            var creneau2 = arrayColonnes[2].children[0].options[arrayColonnes[2].children[0].selectedIndex].value;
+                            var creneau3 = arrayColonnes[3].children[0].options[arrayColonnes[3].children[0].selectedIndex].value;
+                            if (creneau1 !== "Pas de créneau choisi") {
+                                /** ajouter creneau 1 **/
+                                /** on fait une factory avec les paramètres **/
+                                var cren1 = new creneauFactory({
+                                    id: idE,
+                                    heure: creneau1,
+                                    date: date
+                                });
+                                /** on fait un post pour créer le créneau **/
+                                cren1.$save(function success(data){
+                                }, function error(){
+                                    alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau1);
+                                });
+
+                            }
+                            if (creneau2 !== "Pas de créneau choisi") {
+                                /** ajouter creneau 2 **/
+                                /** on fait une factory avec les paramètres **/
+                                var cren2 = new creneauFactory({
+                                    id: idE,
+                                    heure: creneau2,
+                                    date: date
+                                });
+                                /** on fait un post pour créer le créneau **/
+                                cren2.$save(function success(data){
+                                }, function error(){
+                                    alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau2);
+                                });
+                            }
+                            if (creneau3 !== "Pas de créneau choisi") {
+                                /** ajouter creneau 3 **/
+                                /** on fait une factory avec les paramètres **/
+                                var cren3 = new creneauFactory({
+                                    id: idE,
+                                    heure: creneau3,
+                                    date: date
+                                });
+                                /** on fait un post pour créer le créneau **/
+                                cren3.$save(function success(data){
+                                }, function error(){
+                                    alert(status + " - Echec lors de la création du créneau : " + date + " - " + creneau3);
+                                });
+                            }
+                        }
+                        
+                        /** redirection page de détail **/
+                        $location.path('/detailEvenements/'+idE);
+                    }
                 }
             }
             
